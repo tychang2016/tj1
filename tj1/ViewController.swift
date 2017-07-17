9eeb07522512981972b1ca23ec3fc2a9cc2bb4d9
Tung-yao Chang <TY@TYMacPro.local>
2017-07-17 20:51:47 +0800

    Initial Commit

---
+++ tj1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tj1
+//
+//  Created by Tung-yao Chang on 2017/07/17.
+//  Copyright © 2017 Tung-yao Chang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
